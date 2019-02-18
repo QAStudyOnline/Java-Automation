1faec389f67b7d1f608a2e6547fdf5388b3556c8
yemax <egormakismchuk@gmail.com>
2019-02-18 23:14:35 +0200

    Add User DTO

--- 013_lesson/src/main/java/online/qastudy/api/dto/User.java
+++ 013_lesson/src/main/java/online/qastudy/api/dto/User.java
@@ -1,17 +1,196 @@
 package online.qastudy.api.dto;
 
 public class User {
-//    {
-//        "id": 0,
-//            "username": "string",
-//            "firstName": "string",
-//            "lastName": "string",
-//            "email": "string",
-//            "password": "string",
-//            "phone": "string",
-//            "userStatus": 0
-//    }
+    private Long id;
+    private String username;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private String phone;
+    private boolean userStatus;
 
+    public User() {
+    }
 
+    public User(Long id, String username, String firstName, String lastName, String email, String password, String phone, boolean userStatus) {
+        this.id = id;
+        this.username = username;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+        this.phone = phone;
+        this.userStatus = userStatus;
+    }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public boolean isUserStatus() {
+        return userStatus;
+    }
+
+    public void setUserStatus(boolean userStatus) {
+        this.userStatus = userStatus;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+
+        if (userStatus != user.userStatus) return false;
+        if (!id.equals(user.id)) return false;
+        if (!username.equals(user.username)) return false;
+        if (!firstName.equals(user.firstName)) return false;
+        if (!lastName.equals(user.lastName)) return false;
+        if (!email.equals(user.email)) return false;
+        if (!password.equals(user.password)) return false;
+        return phone.equals(user.phone);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id.hashCode();
+        result = 33 * result + username.hashCode();
+        result = 33 * result + firstName.hashCode();
+        result = 33 * result + lastName.hashCode();
+        result = 33 * result + email.hashCode();
+        result = 33 * result + password.hashCode();
+        result = 33 * result + phone.hashCode();
+        result = 33 * result + (userStatus ? 1 : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", phone='" + phone + '\'' +
+                ", userStatus=" + userStatus +
+                '}';
+    }
+
+    public static class UserBuilder {
+        private Long id;
+        private String username;
+        private String firstName;
+        private String lastName;
+        private String email;
+        private String password;
+        private String phone;
+        private boolean userStatus;
+
+        public UserBuilder setId(Long id) {
+            this.id = id;
+            return this;
+        }
+
+        public UserBuilder Username(String username) {
+            this.username = username;
+            return this;
+        }
+
+        public UserBuilder withFirstName(String firstName) {
+            this.firstName = firstName;
+            return this;
+        }
+
+        public UserBuilder withLastName(String lastName) {
+            this.lastName = lastName;
+            return this;
+        }
+
+        public UserBuilder withEmail(String email) {
+            this.email = email;
+            return this;
+        }
+
+        public UserBuilder withPassword(String password) {
+            this.password = password;
+            return this;
+        }
+
+        public UserBuilder withPhone(String phone) {
+            this.phone = phone;
+            return this;
+        }
+
+        public UserBuilder withUserStatus(boolean userStatus) {
+            this.userStatus = userStatus;
+            return this;
+        }
+
+        public User build() {
+            return new User(this.id,
+                    this.username,
+                    this.firstName,
+                    this.lastName,
+                    this.email,
+                    this.password,
+                    this.phone,
+                    this.userStatus);
+        }
+    }
 }
